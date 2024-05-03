8e27b117fef765f25d4d937edcb844e543b0f445
halodad80 <166643978+halodad80@users.noreply.github.com>
2024-05-03 09:01:13 +0000

    fork

---
+++ halodadfork.py
@@ -0,0 +1,31 @@
+name = input("type your name  ")
+print("welcome" , name, "to this adventure!")
+
+character = input("choose a character?  (monk,ranger,mage,rogue ")
+print("you have choosen" , character , "good luck adventurer!") 
+
+if character == "monk" :
+   print (character) == ("monk")
+elif (character) =="ranger" :
+   print (character) == ("ranger")
+elif character== " rogue" :
+    print (character) == ("rogue")
+elif character == "mage" :
+    print (character) == ("mage")
+   
+print("choose your path")    
+    
+__path__ = input("Computing and It, Science, Health Services? ")
+print("path choosen" , __path__)
+if __path__ == "Computing and It" :
+    print (__path__) == ("Computing and It")
+elif (__path__) == "Science" :
+    print (__path__) == ("Science")
+elif (__path__) == "Engineering" :
+    print (__path__) == ("Engineering")
+elif (__path__) == ("Health Science") :
+    print (__path__) == "Health Science"
+
+    
+
+
